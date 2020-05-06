b473d482158f56ab2dc36ec073e48352b32a37fe
Valek <cruiserspb@gmail.com>
2020-05-06 12:31:20 +0300

    deleted comment TEST

--- numbers.c
+++ numbers.c
@@ -42,7 +42,7 @@ int main( int argc, char *argv[] ) {
         } else {
             printf( "iSwapFailed\n" ) ;
         }
-        //end of debug
+        
 
     }
     printf( "A = %ld\nB = %ld\n", A, B ) ;
